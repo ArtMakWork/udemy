eae1ec7bef2a75d530d578dee2dfe46f10a94d54
Artem <artmakwork@gmil.com>
2020-05-24 12:07:19 +0300

    finish gulp file

--- course3/les1/gulpfile.js
+++ course3/les1/gulpfile.js
@@ -1,21 +1,21 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
-const htmlmin = require('gulp-htmlmin');
+const gulp          = require('gulp');
+const browserSync   = require('browser-sync');
+const sass          = require('gulp-sass');
+const cleanCSS      = require('gulp-clean-css');
+const autoprefixer  = require('gulp-autoprefixer');
+const rename        = require("gulp-rename");
+const imagemin      = require('gulp-imagemin');
+const htmlmin       = require('gulp-htmlmin');
 
 gulp.task('server', function() {
-
     browserSync({
         server: {
             baseDir: "dist"
         }
     });
-
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("dist/*.html").on('change', browserSync.reload);
+    gulp.watch("dist/css/*.css").on('change', browserSync.reload);
+    gulp.watch("dist/js/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
@@ -25,12 +25,14 @@ gulp.task('styles', function() {
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
 });
 
 gulp.task('html', function () {
@@ -40,7 +42,7 @@ gulp.task('html', function () {
 });
 
 gulp.task('scripts', function () {
-    return gulp.src("src/js/**/*.js")
+    return gulp.src("src/js/**/*")
         .pipe(gulp.dest("dist/js"));
 });
 
@@ -54,15 +56,10 @@ gulp.task('icons', function () {
         .pipe(gulp.dest("dist/icons"));
 });
 
-gulp.task('mailer', function () {
-    return gulp.src("src/mailer/**/*")
-        .pipe(gulp.dest("dist/mailer"));
-});
-
 gulp.task('images', function () {
     return gulp.src("src/img/**/*")
         .pipe(imagemin())
         .pipe(gulp.dest("dist/img"));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'html', 'images'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
--- course3/les1/src/js/script.js
+++ course3/les1/src/js/script.js
@@ -1,22 +1,36 @@
 'use strict';
 
-const numberOffilms = prompt("Сколько фильмов вы уже посмотрели?");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count : numberOffilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-let ans1 = prompt("Один из последних просмотренных фильмов");
 
-let ans2 = prompt("На сколько оцените его?");
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-let ans3 = prompt("Один из последних просмотренных фильмов");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-let ans4 = prompt("На сколько оцените его?");
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
-personalMovieDB.movies[ans1]=ans2;
-personalMovieDB.movies[ans3]=ans4;
 console.log(personalMovieDB);
