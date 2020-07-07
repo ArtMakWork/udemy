e222144d11263c3fafaf1d8d2fda8a7f44bbba40
Artem <artmakwork@gmil.com>
2020-07-07 22:15:51 +0300

    create got app

---
+++ course3/got/src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return(
+        <div className="loadingio-spinner-double-ring-l9q1d89rr5">
+            <div className="ldio-y2g2hkqlnkr">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
