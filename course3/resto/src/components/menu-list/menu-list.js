f30ffbfedc3950b8a498d30abd77c1bb0ab9e710
Artem <artmakwork@gmil.com>
2020-07-19 19:10:12 +0300

    create window project

--- course3/resto/src/components/menu-list/menu-list.js
+++ course3/resto/src/components/menu-list/menu-list.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
 import { connect } from 'react-redux';
 import WithRestoService from '../hoc';
-import {menuLoaded, menuRequested, menuError} from '../../actions';
+import {menuLoaded, menuRequested, menuError, addedToCart} from '../../actions';
 import Spinner from '../spinner';
-import Error from '../error';
+import Error from '../error'
 
 import './menu-list.scss';
 
@@ -20,7 +20,7 @@ class MenuList extends Component {
     }
 
     render() {
-        const {menuItems, loading, error} = this.props;
+        const {menuItems, loading, error, addedToCart} = this.props;
         if (error){
             return <Error/>
         }
@@ -29,7 +29,10 @@ class MenuList extends Component {
         }
         const items = menuItems.map(menuItem => {
                 return (
-                    <MenuListItem key = {menuItem.id} menuItem = {menuItem}/>
+                    <MenuListItem 
+                        key = {menuItem.id} 
+                        menuItem = {menuItem}
+                        onAddToCart={()=>addedToCart(menuItem.id)}/>
                 )
             })
 
@@ -49,9 +52,10 @@ const mapStateToProps =  (state) =>{
 
 
 const mapDispatchToProps = {
-    menuLoaded: menuLoaded,
+    menuLoaded,
     menuRequested,
-    menuError
+    menuError,
+    addedToCart
 }
 
 
